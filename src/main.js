6809688a8c74ab320f5ff790a6c75fd22adec235
guo.yonghai@cicsc.com <guo.yonghai@cicsc.com>
2018-12-07 14:35:08 +0800

    login

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import './assets/scss/main.scss' /* 引入公共样式 */
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI, { size: 'small' })
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/views/Login/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+    <div class="login-main">
+      <h1 class="login-title">海豚网FMS系统</h1>
+      <div class="login-input-item">
+        <el-input placeholder="请输入登录名">
+          <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        </el-input>
+      </div>
+      <div class="login-input-item">
+        <el-input placeholder="请输入密码" type="password">
+          <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        </el-input>
+      </div>
+      <div class="login-button-item">
+        <el-button type="primary">登录</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="scss" scoped>
+  .login{
+    background-color: #eee;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  .login-main{
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    width: 500px;
+    height: 300px;
+    background-color: #fff;
+    margin: -150px 0 0 -250px;
+    padding: 20px 40px;
+  }
+  .login-title{
+    color: $main-color;
+    line-height: 1em;
+    padding-bottom: 20px;
+  }
+  .login-input-item{
+    width: 100%;
+    height: 32px;
+    margin-bottom: 15px;
+  }
+  .login-button-item{
+    width: 100%;
+    height: 32px;
+  }
+    .login-button-item button{
+      width: 100%;
+    }
+</style>
