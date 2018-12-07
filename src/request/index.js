2c7a519286db13ba1d14f4c5445c4a481a1db539
guo.yonghai@cicsc.com <guo.yonghai@cicsc.com>
2018-12-07 18:27:20 +0800

    login

--- src/request/index.js
+++ src/request/index.js
@@ -1,14 +1,22 @@
 import axios from 'axios'
-import config from 'config'
+// import requestConfig from 'requestConfig'
+const requestConfig = {
+  BASE_URL: 'http://api-fms-test.htw.link/api?action=',
+  TIMEOUT: 30 * 1000
+}
 var service = axios.create({
-  baseURL: config.BASE_URL,
-  timeout: config.TIMEOUT,
+  // baseURL: requestConfig.BASE_URL,
+  timeout: requestConfig.TIMEOUT
 })
 
 // request interceptor
 service.interceptors.request.use(
   config => {
-
+    if (config.action) {
+      config.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+      config.url = requestConfig.BASE_URL + config.action
+    }
+    console.log(config)
     return config
   },
   error => {
@@ -21,11 +29,11 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   response => response,
   error => {
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    })
+    // Message({
+    //   message: error.message,
+    //   type: 'error',
+    //   duration: 5 * 1000
+    // })
     return Promise.reject(error)
   }
 )
